edef7b69cc1aa23148da0e56ea8cb1e9954c1763
Mr.Orange88 <m.orange@gmx.de>
2023-11-10 19:11:03 +0100

    Quiz fertig

--- quiz.js
+++ quiz.js
@@ -9,7 +9,7 @@ let currentQuestionIndex = 0; // Camel Case jedes neues Wort mit Großbuchstaben
 let questions = [ //Das ist eine Variable in dieser Variable ist ein Array mit 3 Objekten in den Objekten sid wieder Array(sammlungen)
 
     {
-        question: "Wie ist mein Name?", //in einem Objetkt werden die Eigenschaften mit einem : zugewiesen!
+        question: "Wie ist mein Name?", //in einem Objekt werden die Eigenschaften mit einem : zugewiesen!
         //           0       1        2       3  der Index fängt bei 0 an zu zählen
         answers: ["Jimmy", "Alex", "Hugo", "Paul"], //Das ist ein Array er enthält 4 Elemente
         correctAnswer: 1
@@ -30,41 +30,25 @@ let questions = [ //Das ist eine Variable in dieser Variable ist ein Array mit 3
         correctAnswer: 1
     },
 ];
-console.log(questions)
-// Frage
-// Mögliche Antworten
-// Richtige Antworten
 
 function displayQuestion() {
 
 
     let item = questions[currentQuestionIndex]; //hiermit sage ich das er mit der ersten Frage anfangen soll(die erste Frage liegt auf Index 0 habe ich oben bestimmt)
     document.getElementById('question').textContent = item.question; //hier hole ich mir das Objekt question(h1 Element) aus der html datei/ mit textcontent sage ich der texinhalt dieses Objekts soll die Frage sein die ich vorher mit question classifiziert habe!
-
-
-    if (currentQuestionIndex >= questions.length) {
-        document.getElementById('quiz-container').hidden = true;
-        document.getElementById('result-container').hidden = false;
-        document.getElementById('result').textContent = result;
-
-    }
 }
 
-
-
 function displayAnswers() {
-    document.getElementById('answers').innerHTML = ""; //damit clearen wir den container
-    let item = questions[currentQuestionIndex]; // Zuweisung
+    document.getElementById('answers').innerHTML = ""; //clear container
+    let item = questions[currentQuestionIndex];
     for (let answerIndex = 0; answerIndex < item.answers.length; answerIndex = answerIndex + 1) // Abkürzung wäre in dem Fall ++
     {
-        let button = document.createElement('button');// w
+        let button = document.createElement('button');
         button.classList.add("btn", "btn-dark", "me-2");
-        button.textContent = item.answers[answerIndex];// TODO answerIndex
+        button.textContent = item.answers[answerIndex];
         button.onclick = function (event) {
-
             checkAnswer(item, answerIndex);
         }
-
 
         document.getElementById('answers').appendChild(button)
     }
@@ -73,18 +57,34 @@ function displayAnswers() {
 function checkAnswer(item, answerIndex) {
     if (answerIndex === item.correctAnswer) {
         result++;
-
-
     }
 
-    //ToDo Wenn das Game beendet zeige Result - Container
-    //ToDo Zeige Ergebnis Result Element 
-    //ToDo sonst next Question (else) 
-    nextQuestion();
-}
+    if (currentQuestionIndex === questions.length - 1) {
+        displayResult();
+    } else {
+        nextQuestion();
+    }
 
+}
+function displayResult() {
+    //die Property hidden wird in neuen Browsern unterstützt
+    //für alte Browser nehmen style.display
+    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden#browser_compatibility
+    document.getElementById('quiz-container').hidden = true;
+    document.getElementById('quiz-container').style.display = "none";
+    document.getElementById('result-container').hidden = false;
+    document.getElementById('result-container').style.display = "block";
+    document.getElementById('result').textContent = result;
+}
+function displayGame() {
+    document.getElementById('quiz-container').hidden = false;
+    document.getElementById('quiz-container').style.display = "block";
+    document.getElementById('result-container').hidden = true;
+    document.getElementById('result-container').style.display = "none";
+    document.getElementById('result').textContent = "";
+}
 function nextQuestion() {
-    if (currentQuestionIndex < questions.length - 1) { //Bedingung
+    if (currentQuestionIndex < questions.length - 1) {
         currentQuestionIndex++;
         displayQuestion();
         displayAnswers();
@@ -98,13 +98,13 @@ function previousQuestion() {
         displayAnswers();
     }
 }
-function restart() {
-    if (currentQuestionIndex >= questions.length) {
-        displayQuestion();
-        displayAnswers();
-    }
+function restartGame() {
+    currentQuestionIndex = 0;
+    displayGame();
+    displayQuestion();
+    displayAnswers();
 }
-//ToDo Hier Game Reset Funktion einfügen
+
 document.getElementById('next-button').onclick = function (event) {
     nextQuestion();
 }
@@ -114,11 +114,9 @@ document.getElementById('previous-button').onclick = function (event) {
 }
 
 document.getElementById('restart-button').onclick = function (event) {
-    restart();
+    restartGame();
 }
-// ToDo Hier onclick funktion für Reset Button einfügen
-displayQuestion(); //hiermit rufe ich die funktion auf die ich vorher definiert habe! 
-displayAnswers();
+restartGame();
 
 
 
